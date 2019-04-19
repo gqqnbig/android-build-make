68c065ba05d367c9e3bd586dc0c2ce6e050013c8
gqqnbig <gqqnb2005@gmail.com>
2019-04-18 17:23:45 -0700

    改进
    Change-Id: Id5214695a2f7ad3d81e3e484e97260915b5fecbf

--- flash.sh
+++ flash.sh
@@ -1,21 +1,45 @@
 #!/bin/bash
 
+# 如果任何命令状态码不是0，就停止执行本脚本
+set -e
+
 
 waitForHomeScreen ()
 {
+	set +e
 	x=$(adb wait-for-device shell getprop sys.boot_completed)
 	while [ "$x" != "1" ]; do
 		sleep 2
-		read -t 0.1 -n 1 -s -r -p  $'\rWaiting for home screen. Press any key to stop waiting and continue to executing the following commands.\r'
+		read -t 0.1 -n 1 -s -r -p  $'\rWaiting for home screen. Press any key to stop waiting and continue to executing the following commands.'
 		if [[ $? -eq 0 ]]; then
+			sleep 2
 			break
 		fi
 		x=$(adb shell getprop sys.boot_completed)
 	done
 	echo ""
+	set -e
+}
+
+waitForRecoveryDesktop ()
+{
+	set +e
+	x=$(adb wait-for-device shell getprop twrp.action_complete)
+	while [[ -z "$x" ]]; do
+		sleep 2
+		read -t 0.1 -n 1 -s -r -p  $'\rWaiting for recovery mode desktop. Press any key to stop waiting and continue to executing the following commands.'
+		if [[ $? -eq 0 ]]; then
+			sleep 2
+			break
+		fi
+		x=$(adb shell getprop twrp.action_complete)
+	done
+	echo ""
+	set -e
 }
 
 read -n 1 -s -r -p "Make user the phone is in fastboot mode, then press any key to continue"
+echo ""
 #Try to only flash system partition, thus don't have to flash TWRP recovery image over and over.
 
 # 有时候不能很好地运行
@@ -30,14 +54,21 @@ adb push Magisk-v18.1.zip /sdcard/
 adb push open_gapps-arm64-8.1-mini-20190409.zip /sdcard/
 # Install root manager app
 adb install MagiskManager-v7.1.1.apk
-echo "Open Magisk and test."
+echo "The phone is not rooted yet. Open Magisk Manager, it should say Magisk is not Installed."
 read -n 1 -s -r -p "Press any key to continue"
+echo ""
 
 adb reboot recovery
+adb wait-for-recovery shell getprop
+waitForRecoveryDesktop
+
 adb shell twrp install /sdcard/Magisk-v18.1.zip
 
 
-fastboot reboot
+# 在TWRP环境
+adb reboot
+
+waitForHomeScreen
 
 echo "adb shell is going to execute for the first time. Pay attention to Magisk prompt and grant access."
 echo "Change Request Timeout to a larger value if you wish."
@@ -47,5 +78,7 @@ adb shell su -c 'svc bluetooth disable'
 adb shell settings put system accelerometer_rotation 0 #disable auto rotate
 
 adb reboot recovery
-adb wait-for-recovery shell twrp install open_gapps-arm64-8.1-mini-20190409.zip
+adb wait-for-recovery
+waitForRecoveryDesktop
+adb shell twrp install open_gapps-arm64-8.1-mini-20190409.zip
 adb reboot
