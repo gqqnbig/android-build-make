08efcb7bd956f3539659370f2c10784e1972004b
gqqnbig <gqqnb2005@gmail.com>
2019-05-21 20:58:48 -0700

    可以正确输出日志了
    Change-Id: I583f621dcb44125d293107026a4bc136a09531ea

--- start-profiling.sh
+++ start-profiling.sh
@@ -1,12 +1,9 @@
-pid=120
-adb shell "sh -c 'echo $$; echo hello; exec app_process -Djava.class.path=/sdcard/Hello.dex -EnableRWProfiling:true -EnableHeapSizeProfiling:true /sdcard/ Hello '" | {
-  while IFS= read -r line
-  do
-		pid=$line
-		echo "pid is $pid"
-		break
-  done
+# echo $$ 里的$$在调用时展开，所以如果不用单引号包起来，展开的是当前shell的PID。
+# 同理，sh -c的参数也必须用单引号包起来。
+adb shell 'sh -c '"'"'echo $$; sleep 1; exec app_process -Djava.class.path=/sdcard/Hello.dex -EnableRWProfiling:true -EnableHeapSizeProfiling:true /sdcard/ Hello '"'" | {
+	IFS= read -r line
+	pid=$line
+	echo "pid is $pid. Start capturing..."
+
+	rogcat -m '\[HT\]'  -f json | grep -F "process\":\"$pid\"" > trace.json
 }
-echo "pid is $pid"
-
-
